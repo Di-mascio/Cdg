b33a4355bd871232a57d8e70cccbbced138d2590
Maxime <robin.maxime@lyceeppr.fr>
2021-02-09 08:57:05 +0100

    ajout de graphique

--- cdg/mainwindow.cpp
+++ cdg/mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->label->setStyleSheet(QString::fromUtf8("background-color: rgb(144, 28, 58);"));
+    ui->label2->setStyleSheet(QString::fromUtf8("background-color: rgb(197, 29, 74);"));
+    QPixmap monImage("/home/maxime/Images/");
+    QLabel* m_label = new QLabel();
+        m_label->setPixmap(monImage);
 }
 
 MainWindow::~MainWindow()
